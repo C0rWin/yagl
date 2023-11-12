9471495ce2008ddc6084f42597dc268d39b25f8c
Artem Barger <artem@bargr.net>
2023-11-12 15:40:18 +0200

    Optimize unit-test and make sure logger preserves level hierarchy
    Signed-off-by: Artem Barger <artem@bargr.net>

--- logger_test.go
+++ logger_test.go
@@ -7,45 +7,19 @@ import (
 	"github.com/stretchr/testify/require"
 )
 
-func TestLogger(t *testing.T) {
-	tests := []struct {
-		name     string
-		message  string
-		logLevel Setting
-		log      func(l *Logger)
-		assert   func(t *testing.T, actual, expected string)
-	}{
-		{
-			name:     "Testing default logger trying to print a message with INFO level",
-			message:  "Hello World",
-			logLevel: Level(Info),
-			log: func(l *Logger) {
-				l.Logf(Info, "Hello World")
-			},
-			assert: func(t *testing.T, actual, expected string) {
-				require.Contains(t, actual, expected)
-				require.Contains(t, actual, Info.String())
-			},
-		},
-		{
-			name:     "Testing default logger trying to print a message with DEBUG level",
-			message:  "Hello World",
-			logLevel: Level(Info),
-			log: func(l *Logger) {
-				l.Logf(Debug, "Hello World")
-			},
-			assert: func(t *testing.T, actual, expected string) {
-				require.Empty(t, actual)
-			},
-		},
-	}
-
-	for _, test := range tests {
-		t.Run(test.name, func(t *testing.T) {
-			buffer := bytes.NewBuffer(nil)
-			logger := New(CustomLogOut(buffer), StdFormat, test.logLevel)
-			test.log(logger)
-			test.assert(t, buffer.String(), test.message)
-		})
+func TestLoggerHierarchy(t *testing.T) {
+	for _, logLevel := range []LogLevel{Debug, Info, Warn, Error} {
+		for _, messageLogLevel := range []LogLevel{Debug, Info, Warn, Error} {
+			t.Run(logLevel.String()+"-"+messageLogLevel.String(), func(t *testing.T) {
+				buffer := bytes.NewBuffer(nil)
+				logger := New(CustomLogOut(buffer), StdFormat, Level(logLevel))
+				logger.Logf(messageLogLevel, "Hello World")
+				if messageLogLevel >= logLevel {
+					require.Contains(t, buffer.String(), "Hello World")
+				} else {
+					require.Empty(t, buffer.String())
+				}
+			})
+		}
 	}
 }
